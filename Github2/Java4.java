b27f0fc64d046118a93632e9a289c95bff8aaad6
Ubuntu <ubuntu@ip-172-31-33-123.us-east-2.compute.internal>
2018-10-27 04:16:11 +0000

    Java4.java

---
+++ Github2/Java4.java
@@ -0,0 +1,21 @@
+void main()
+{
+  public class
+  {
+    puts("JavA4");
+    gets("java4 read");
+  }
+}
+
+
+
+void main()
+{
+ public sub()
+ {
+   puts(" Java 4 Sub");
+   gets(" Java 4 Sub read"):
+ }
+}
+
+
